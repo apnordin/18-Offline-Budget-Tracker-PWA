71cd21dbe2d115d13a43a79307bb91db7c37f464
Alex Nordin <apnordin@gmail.com>
2021-01-23 11:59:32 -0500

    Offline still working, but does not sync when going bak online

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,15 @@ console.log("Your service-worker.js file is working");
 // Set up cache files
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest"
+];
 
 
 // Install service worker
@@ -14,7 +22,7 @@ self.addEventListener("install", function(evt) {
         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
 
-    // Pre-cache all static assets. This sentence makes sense because we did a thorough walkthrough, explanation, and demo in class. We definitely didn't just do this activity by ourselves for 20 minutes and then move on with no recap.
+    // Pre-cache all static assets
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
@@ -44,16 +52,18 @@ self.addEventListener("activate", function(evt) {
 });
 
 
-//Enable the service worker the intercept network requests
+// Modify our service worker to handle requests to /api, store those responses in our cache to access later
 
-self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api')) {
+self.addEventListener("fetch", function(evt) {
+    // Cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
         console.log('[Service Worker] Fetch (data)', evt.request.url);
 
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                 .then(response => {
+                    // If the response was good, clone it and store it in the cache
                     if (response.status === 200) {
                         cache.put(evt.request.url, response.clone());
                     }
@@ -61,10 +71,11 @@ self.addEventListener('fetch', function(evt) {
                     return response;
                 })
                 .catch(err => {
+                    // If network request fails, try to get it from the cache
                     return cache.match(evt.request);
                 });
-            })
-        )
+            }).catch(err => console.log(err))
+        );
 
         return;
     }
